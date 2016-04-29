49f5577155725fd541651059eeea2235d1f857d6
jkosturko <jessica.kosturko@gmail.com>
2016-04-29 12:37:32 -0400

    Running Node Study Hall: Hello World

--- program.js
+++ program.js
@@ -1,15 +1,4 @@
+console.log('HELLO WORLD');
 
-var fs = require('fs');
-var path = require('path');
-
-module.exports = fs.readdir(process.argv[2], function (err,list) {
-
-    for(var i=0; i <list.length; i++) {
-
-        if ( path.extname(list[i]) === '.' + process.argv[3]) {
-            console.log(list[i]);
-        }
-    }
-});
 
 
