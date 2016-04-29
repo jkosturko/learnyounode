4fabc22c627ce11e9171b86f299d2c2e7301fcfa
jkosturko <jessica.kosturko@gmail.com>
2016-04-29 12:44:05 -0400

    BABY STEPS: Node Study Hall

--- program.js
+++ program.js
@@ -1,4 +1,11 @@
-console.log('HELLO WORLD');
+// console.log(process.argv)
+
+var sum = 0;
+for (var i = 2; i < process.argv.length; i += 1) {
+    sum += +process.argv[i];
+}
+
+console.log(sum);
 
 
 
